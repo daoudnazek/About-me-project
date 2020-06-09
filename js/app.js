8a1e82bb5261e77601f2cc3f592a6afb583a7bf5
daoudnazek <dawood_nazek_92@hotmail.com>
2020-06-10 00:40:50 +0300

    fix if statement error

--- js/app.js
+++ js/app.js
@@ -6,12 +6,12 @@ alert('Please try to guess the following five questions by answering y/yes or n/
 
 var myAge = prompt('I am in my 30s, yes or no ?');
 
-if(myAge.toLowerCase() == 'yes' || 'y'){
+if(myAge.toLowerCase() == 'yes' || myAge.toLowerCase() =='y'){
     alert('oops, wrong answer! I am in my 20s');
     console.log('I am in my 30s, yes or no ?');
     console.log('you answered yes, but I am not, I am in my 20s');
     
-}else if(myAge.toLowerCase() == 'no' || 'n'){
+}else if (myAge.toLowerCase() == 'no' || myAge.toLowerCase() =='n'){
     alert('yes, that is correct! I am in my 20s');
     console.log('I am in my 30s, yes or no ?');
     console.log('yes that is correct');
@@ -19,11 +19,11 @@ if(myAge.toLowerCase() == 'yes' || 'y'){
 
 var myStudies = prompt('Am I an engineer?');
 
-if(myStudies.toLowerCase() == 'yes' || 'y'){
+if(myStudies.toLowerCase() == 'yes' || myStudies.toLowerCase() == 'y'){
     alert('yes, that is correct answer :)');
     console.log('Am I an engineer?');
     console.log('you answered yes and that is correct')
-} else if (myStudies.toLowerCase() == 'no' || 'n'){
+} else if (myStudies.toLowerCase() == 'no' || myStudies.toLowerCase() =='n'){
     alert('oops, wrong answer! I am an engineer')
     console.log('Am I an engineer?');
     console.log('you answered No, but I am an engineer');
@@ -31,12 +31,12 @@ if(myStudies.toLowerCase() == 'yes' || 'y'){
 
 var mylastWork = prompt('Did I work in e-commerce bussines?');
 
-if(mylastWork.toLowerCase == 'yes' || 'y'){
+if(mylastWork.toLowerCase() == 'yes' || mylastWork.toLowerCase() == 'y'){
     alert('yes, that is correct :)')
     console.log('Did I work in e-commerce bussines?')
     console.log('you answered yes and that is correct')
 
-}else if (mylastWork.toLowerCase() == 'no' || 'n'){
+}else if (mylastWork.toLowerCase() == 'no' || mylastWork.toLowerCase() == 'n'){
     alert('oops, wrong answer! I did')
     console.log('Did I work in e-commerce bussines?')
     console.log('you answered No, but I did');
@@ -44,12 +44,12 @@ if(mylastWork.toLowerCase == 'yes' || 'y'){
 
 var myOtherlastWork = prompt('Do I know about photographing?');
 
-if(myOtherlastWork.toLowerCase == 'yes' || 'y'){
+if(myOtherlastWork.toLowerCase() == 'yes' || myOtherlastWork.toLowerCase() == 'y'){
     alert('yes, that is correct :)')
     console.log('Do I know about photographing?')
     console.log('you answered yes and that is correct')
 
-}else if (myOtherlastWork.toLowerCase == 'no' || 'n'){
+}else if (myOtherlastWork.toLowerCase() == 'no' || myOtherlastWork.toLowerCase() =='n'){
     alert('oops, wrong answer! I do')
     console.log('Do I know about photographing?')
     console.log('you answered No, but I do');
@@ -57,13 +57,13 @@ if(myOtherlastWork.toLowerCase == 'yes' || 'y'){
 
 var programmingStartingYear = prompt('Did I start studying programming in 2019?');
 
-if(programmingStartingYear.toLowerCase == 'yes' || 'y'){
+if(programmingStartingYear.toLowerCase() == 'yes' || programmingStartingYear.toLowerCase() == 'y'){
     alert('oops, wrong answer! I started in 2020');
     console.log('Did I start studying programming in 2019?');
     console.log('you answered yes, but I did');
 
 
-}else if (programmingStartingYear.toLowerCase == 'no' || 'n'){
+}else if (programmingStartingYear.toLowerCase() == 'no' || programmingStartingYear.toLowerCase() == 'n'){
     alert('yes, that is correct :)')
     console.log('Did I start studying programming in 2019?');
     console.log('you answered no and that is correct')
@@ -71,3 +71,19 @@ if(programmingStartingYear.toLowerCase == 'yes' || 'y'){
 }
 
 alert('Thank you ' + userName + ' ,you have tried your best')
+
+
+// for (var i =0; i<4 ; i++){
+//     var guessNumber = prompt('would you guess the correct number? you have four attempts');
+
+// if (guessNumber > 7){
+//     alert('it`s too high');
+// }else if(guessNumber < 7){
+//     alert('it`s too low')
+// }
+// }
+
+// alert('The right number is 7')
+
+
+// var x=0;
