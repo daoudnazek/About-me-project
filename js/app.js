7c1de90f99309ec4577eab565563407575c69321
daoudnazek <dawood_nazek_92@hotmail.com>
2020-06-09 05:52:29 +0300

    welcoming the user by own name

---
+++ js/app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var userName = prompt('please enter your name');
+alert('Hello ' + userName + ' , welcome to my website');
+alert('please try to guess the following five questions by answering y/yes or n/no')
